7dcad8ca72b523f6be846d33710f22ac80436962
AJ <anthonyjbell21@gmail.com>
2020-01-25 18:13:22 -0800

    fixed syntax float to string

--- helloJenkins.py
+++ helloJenkins.py
@@ -4,4 +4,4 @@ import yfinance as yf
 msft = yf.Ticker("SQ")
 
 # get stock info
-print(msft.info["symbol"] + ": " + msft.info["regularMarketPrice"])
+print(msft.info["symbol"] + ": " + str(msft.info["regularMarketPrice"]))
